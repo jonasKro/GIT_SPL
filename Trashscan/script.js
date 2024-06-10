0eb6e02d68f481fc88581cfd4e8ca07a06e77e3e
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2024-06-10 10:16:27 +0200

    Umgebaut von Google Maps Api auf HERE API

--- Trashscan/script.js
+++ Trashscan/script.js
@@ -71,97 +71,83 @@ function SimpleTSP() {
 }
 
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 47.4125, lng: 9.74355 },
-    zoom: 13,
-    styles: [
-      {
-        featureType: "poi",
-        elementType: "labels",
-        stylers: [{ visibility: "off" }],
-      },
-      {
-        featureType: "road",
-        elementType: "labels",
-        stylers: [{ visibility: "off" }],
-      },
-      {
-        featureType: "transit",
-        elementType: "labels",
-        stylers: [{ visibility: "off" }],
-      },
-    ],
+  const platform = new H.service.Platform({
+    apikey: "N0gPRVOQOQACbb8IUQ8mC9l3P_COW1JxYcWhzWwHJeo",
   });
 
-  let red = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
-  let green = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
-  let blue = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+  const defaultLayers = platform.createDefaultLayers();
+  const map = new H.Map(
+    document.getElementById("map"),
+    defaultLayers.vector.normal.map,
+    {
+      center: { lat: 47.4125, lng: 9.74355 },
+      zoom: 14,
+      pixelRatio: window.devicePixelRatio || 1,
+    }
+  );
+
+  const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  const ui = H.ui.UI.createDefault(map, defaultLayers);
+
+  let redIcon = new H.map.Icon("red.png");
+  let greenIcon = new H.map.Icon("green.png");
+  let blueIcon = new H.map.Icon("blue.png");
+
   const markers = [
     {
       lat: 47.401391236282066,
       lng: 9.751958834731964,
       title: "Start",
-      icon: blue,
+      icon: blueIcon,
     },
-    { lat: 47.4125, lng: 9.74355, title: "Marker 1", icon: red },
-    //  { lat: 47.415, lng: 9.745, title: "Marker 2", icon: green },
-    { lat: 47.418, lng: 9.747, title: "Marker 3", icon: red },
-    { lat: 47.42, lng: 9.749, title: "Marker 4", icon: red },
-    { lat: 47.422, lng: 9.751, title: "Marker 5", icon: green },
-    { lat: 47.424, lng: 9.753, title: "Marker 6", icon: green },
+    { lat: 47.4125, lng: 9.74355, title: "Marker 1", icon: redIcon },
+    { lat: 47.418, lng: 9.747, title: "Marker 3", icon: redIcon },
+    { lat: 47.42, lng: 9.749, title: "Marker 4", icon: redIcon },
+    { lat: 47.422, lng: 9.751, title: "Marker 5", icon: greenIcon },
+    { lat: 47.424, lng: 9.753, title: "Marker 6", icon: greenIcon },
     {
       lat: 47.409602701097,
       lng: 9.724056374784443,
       title: "Marker 8",
-      icon: green,
+      icon: greenIcon,
     },
     {
       lat: 47.41000393653498,
       lng: 9.743496003903328,
       title: "Marker 7",
-      icon: red,
+      icon: redIcon,
     },
-    /*    {
-      lat: 47.4107193803123,
-      lng: 9.733852370986181,
-      title: "Marker 9",
-      icon: green,
-    }, */
     {
       lat: 47.42478185167308,
       lng: 9.726834342681482,
       title: "Marker 10",
-      icon: red,
+      icon: redIcon,
     },
     {
       lat: 47.43410765032678,
       lng: 9.741204591191707,
       title: "Marker 11",
-      icon: green,
+      icon: greenIcon,
     },
     {
       lat: 47.422068581745755,
       lng: 9.748640359570645,
       title: "Marker 12",
-      icon: red,
+      icon: redIcon,
     },
   ];
 
   markers.forEach((markerInfo) => {
-    const marker = new google.maps.Marker({
-      position: { lat: markerInfo.lat, lng: markerInfo.lng },
-      map: map,
-      title: markerInfo.title,
-      icon: markerInfo.icon,
-    });
+    const marker = new H.map.Marker(
+      { lat: markerInfo.lat, lng: markerInfo.lng },
+      { icon: markerInfo.icon }
+    );
+    map.addObject(marker);
   });
 
   const redMarkerPositions = markers
-    .filter((markerInfo) => markerInfo.icon === red)
-    .map((markerInfo) => ({
-      lat: markerInfo.lat,
-      lng: markerInfo.lng,
-    }));
+    .filter((markerInfo) => markerInfo.icon === redIcon)
+    .map((markerInfo) => ({ lat: markerInfo.lat, lng: markerInfo.lng }));
 
   const startingPoint = { lat: 47.401391236282066, lng: 9.751958834731964 };
 
@@ -174,99 +160,36 @@ function initMap() {
   const waypoints = optimizedOrder
     .slice(1, optimizedOrder.length - 1)
     .map((position) => ({
-      location: position,
-      stopover: true,
+      lat: position.lat,
+      lng: position.lng,
     }));
 
-  const directionsService = new google.maps.DirectionsService();
-  const directionsRenderer = new google.maps.DirectionsRenderer({
-    map: map,
-    suppressMarkers: true,
-  });
-
-  const request = {
-    origin: waypoints[0].location,
-    destination: waypoints[waypoints.length - 1].location,
-    waypoints: waypoints.slice(1),
-    travelMode: google.maps.TravelMode.DRIVING,
+  const router = platform.getRoutingService(null, 8);
+  const routeRequestParams = {
+    mode: "fastest;car",
+    representation: "display",
+    waypoint0: `geo!${waypoints[0].lat},${waypoints[0].lng}`,
+    waypoint1: `geo!${waypoints[waypoints.length - 1].lat},${
+      waypoints[waypoints.length - 1].lng
+    }`,
+    waypoint2: waypoints
+      .slice(1)
+      .map((waypoint, index) => `geo!${waypoint.lat},${waypoint.lng}`)
+      .join("|"),
   };
 
-  directionsService.route(request, function (response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsRenderer.setDirections(response);
-    } else {
-      window.alert("Directions request failed due to " + status);
-    }
-  });
-
-  let ammountRed = 0;
-  let ammountGreen = 0;
-  markers.forEach((markerInfo) => {
-    if (markerInfo.icon == red) {
-      ammountRed++;
-    }
-    if (markerInfo.icon == green) {
-      ammountGreen++;
-    }
-  });
-
-  document.getElementById("redBins").innerHTML =
-    "<div class='red'>" + ammountRed + "</div>" + "&nbsp;&nbsp;bins are full.";
-  document.getElementById("greenBins").innerHTML =
-    "<div class='green'>" +
-    ammountGreen +
-    "</div>" +
-    "&nbsp;&nbsp;bins aren't full.";
-  document.getElementById("totalBins").innerHTML =
-    "<div class='total'>" +
-    (ammountGreen + ammountRed) +
-    "</div>" +
-    "&nbsp;&nbsp;active bins.";
-
-  directionsService.route(request, function (response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsRenderer.setDirections(response);
-
-      const route = response.routes[0];
-      const legs = route.legs;
-
-      let totalDistance = 0;
-      let totalTime = 0;
-
-      for (let i = 0; i < legs.length; i++) {
-        totalDistance += legs[i].distance.value;
-        totalTime += legs[i].duration.value;
-      }
-
-      const distanceInKm = (totalDistance / 1000).toFixed(2);
-
-      const hours = Math.floor(totalTime / 3600);
-      const minutes = Math.floor((totalTime % 3600) / 60);
-
-      document.getElementById("distance").innerHTML =
-        "Distance: " + distanceInKm + " km";
-      document.getElementById("duration").innerHTML =
-        "Estimated Time: " + hours + "h " + minutes + "min";
-    } else {
-      window.alert("Directions request failed due to " + status);
-    }
-  });
+  router.calculateRoute(routeRequestParams, onSuccess, onError);
 }
+function onSuccess(result) {
+  const route = result.response.route[0];
+  const totalDistance = route.summary.distance / 1000; // in kilometers
+  const totalTime = route.summary.travelTime / 60 / 60; // in hours
+  const distanceInKm = totalDistance.toFixed(2);
+  const hours = Math.floor(totalTime);
+  const minutes = Math.floor((totalTime - hours) * 60);
 
-function arePositionsEqual(pos1, pos2) {
-  return pos1.lat === pos2.lat && pos1.lng === pos2.lng;
+  document.getElementById("distance").innerHTML =
+    "Distance: " + distanceInKm + " km";
+  document.getElementById("duration").innerHTML =
+    "Estimated Time: " + hours + "h " + minutes + "min";
 }
-
-var today = new Date();
-var date =
-  today.getDate() + "." + (today.getMonth() + 1) + "." + today.getFullYear();
-var hours = today.getHours();
-var minutes = today.getMinutes();
-
-var hoursString = (hours < 10 ? "0" : "") + hours;
-var minutesString = (minutes < 10 ? "0" : "") + minutes;
-
-var time = hoursString + ":" + minutesString;
-var dateTime = date + " " + time;
-
-document.getElementById("date").innerHTML = dateTime;
