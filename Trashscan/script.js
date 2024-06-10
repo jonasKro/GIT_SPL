1ab8e31b633ca17597370a92b67ce9cd12c40284
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2024-06-10 11:09:01 +0200

    Marker funktioniert endlich

--- Trashscan/script.js
+++ Trashscan/script.js
@@ -22,74 +22,36 @@ function initMap() {
       lat: 47.401391236282066,
       lng: 9.751958834731964,
       title: "Start",
-      icon: "blue",
+      icon: new H.map.Icon(
+        "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+      ),
     },
     {
       lat: 47.4125,
       lng: 9.74355,
       title: "Marker 1",
-      icon:
-        "https://developer.here.com/documentation/maps/topics/markers/img/red-dot.png",
+      icon: new H.map.Icon(
+        "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+      ),
     },
     {
       lat: 47.418,
       lng: 9.747,
       title: "Marker 3",
-      icon:
-        "https://developer.here.com/documentation/maps/topics/markers/img/red-dot.png",
+      icon: new H.map.Icon(
+        "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+      ),
     },
-    {
-      lat: 47.42,
-      lng: 9.749,
-      title: "Marker 4",
-      icon:
-        "https://developer.here.com/documentation/maps/topics/markers/img/red-dot.png",
-    },
-    { lat: 47.422, lng: 9.751, title: "Marker 5", icon: "green" },
-    { lat: 47.424, lng: 9.753, title: "Marker 6", icon: "green" },
-    {
-      lat: 47.409602701097,
-      lng: 9.724056374784443,
-      title: "Marker 8",
-      icon: "green",
-    },
-    {
-      lat: 47.41000393653498,
-      lng: 9.743496003903328,
-      title: "Marker 7",
-      icon: "red",
-    },
-    {
-      lat: 47.42478185167308,
-      lng: 9.726834342681482,
-      title: "Marker 10",
-      icon: "red",
-    },
-    {
-      lat: 47.43410765032678,
-      lng: 9.741204591191707,
-      title: "Marker 11",
-      icon: "green",
-    },
-    {
-      lat: 47.422068581745755,
-      lng: 9.748640359570645,
-      title: "Marker 12",
-      icon: "red",
-    },
+    // Weitere Marker hier hinzufügen
   ];
 
-  /*  markers.forEach((markerInfo) => {
-    const icon = new H.map.Icon(
-      `https://developer.here.com/documentation/maps/topics/markers/img/${markerInfo.icon}-dot.png`
-      
-    );
+  markers.forEach((markerInfo) => {
     const marker = new H.map.Marker(
       { lat: markerInfo.lat, lng: markerInfo.lng },
-      { icon: icon }
+      { icon: markerInfo.icon }
     );
     map.addObject(marker);
-  });*/
+  });
 }
 
 initMap();
